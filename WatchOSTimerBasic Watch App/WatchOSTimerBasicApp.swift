f251bcaf6336597234556f7f72c07be6ebd76f34
Kraig Kistler <kraigwkistler@gmail.com>
2023-04-09 15:45:21 -0700

    Initial Commit

---
+++ WatchOSTimerBasic Watch App/WatchOSTimerBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSTimerBasicApp.swift
+//  WatchOSTimerBasic Watch App
+//
+//  Created by Kraig Kistler on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSTimerBasic_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
